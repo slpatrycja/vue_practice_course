82412a21980af29ab3942cd85a4f90877e9eb8c9
slpatrycja <sl.patrycja.of@gmail.com>
2019-06-24 23:51:55 +0200

    Bootstrap setup

--- udemy/community-blog/src/App.vue
+++ udemy/community-blog/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
+    <Navbar/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import Navbar from '@/components/Navbar'
+  export default {
+    name: 'app',
+    components: {
+      Navbar
+    }
   }
-}
 </script>
 
 <style>
@@ -22,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
